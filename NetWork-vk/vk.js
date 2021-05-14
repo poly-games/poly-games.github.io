f26f798a7cb56e97aca4bdf1bb0b91d73813e94c
poly-games <84047028+poly-games@users.noreply.github.com>
2021-05-14 19:11:16 +0300

    Add files via upload

--- NetWork-vk/vk.js
+++ NetWork-vk/vk.js
@@ -7,6 +7,6 @@
 }, '5.130');
 
 publish(level_id){
-	VK.api("wall.post", {"message": "Я прошел " + level_id + " уровень в игре NetWork95! А ты сможешь? ", "attachments" : "photo-204575851_457239018, https://vk.com/app7851988_649233139" "v":"5.73"}, function (data) {
+	VK.api("wall.post", {"message": 'Я прошел ' + level_id + ' уровень в игре NetWork95! А ты сможешь? ', "attachments" : "photo-204575851_457239018,https://vk.com/app7851988_649233139" "v":"5.73"}, function (data) {
 	alert("Post ID:" + data.response.post_id);
 }
