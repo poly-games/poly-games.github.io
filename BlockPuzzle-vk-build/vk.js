5f494e455442bd54212df27aebb6cea5a94c4245
poly-games <84047028+poly-games@users.noreply.github.com>
2021-05-14 22:20:38 +0300

    Add files via upload

---
+++ BlockPuzzle-vk-build/vk.js
@@ -0,0 +1,18 @@
+  VK.init(function() {
+     // API initialization succeeded
+     // Your code here
+  }, function() {
+     alert("ошибка вк")
+}, '5.130');
+
+
+function publish(level_id){
+	VK.api("wall.post", {"message": 'Я собрал ' + level_id + ' паззлов в игре Block Puzzle! А сможешь ли ты?', "attachments" : "https://vk.com/app7851988_649233139", "v":"5.73"}, function (data) {
+		//;
+	});
+};
+
+
+function progress(level_id){
+	VK.api("secure.addAppEvent", {"activity_id":"1", "value":level_id})
+}
